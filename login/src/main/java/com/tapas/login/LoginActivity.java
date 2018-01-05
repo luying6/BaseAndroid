3e85f42666982323d63c14169d1df229c02514fc
luying <2225745210@qq.com>
2018-01-05 15:34:02 +0800

    合并Router分支

--- login/src/main/java/com/tapas/login/LoginActivity.java
+++ login/src/main/java/com/tapas/login/LoginActivity.java
@@ -3,7 +3,6 @@ package com.tapas.login;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 /**
  * 创建人：luying
@@ -16,6 +15,5 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
-        Toast.makeText(this, "LgoinActivity", Toast.LENGTH_SHORT).show();
     }
 }
